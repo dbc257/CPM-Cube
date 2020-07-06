bd136cdb523e8848a840949c69c1b10638d503e0
Victor Petsev <Vpetsev96@gmail.com>
2020-07-06 15:52:06 -0500

    Readme and Navbar added

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,22 +1,25 @@
-import React from "react"
-import Navbar from 'react-bootstrap/Navbar'
+import React from "react";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from "react-bootstrap/Nav";
 
 export default function NavBar() {
     return (
-      <>
-        <Navbar bg="dark" variant="dark">
-          <Navbar.Brand href="#home">
-            <img
-              alt=""
-              src="/logo.svg"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{" "}
-            React Bootstrap
-          </Navbar.Brand>
-        </Navbar>
-      </>
-    );
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+                <Nav.Link href="#features">Home</Nav.Link>
+                <Nav.Link href="#pricing">About</Nav.Link>
+            </Nav>
+            <Nav>
+                <Nav.Link href="#deets">Register</Nav.Link>
+                <Nav.Link eventKey={2} href="#memes">
+                    Login
+            </Nav.Link>
+        </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+    )
 }
 
