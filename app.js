030150f6c669f5c794df25e0fb7d0c24367adaf5
David Carlisle <dave_0021@yahoo.com>
2020-07-07 13:19:30 -0500

    fixed issues

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // const FinanceMockDBRouter = require("./routes/api-mock");
