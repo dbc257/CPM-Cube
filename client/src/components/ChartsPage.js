c9a74db51ef8dd2cd2d8325811f0c1efa229681b
David Carlisle <dave_0021@yahoo.com>
2020-07-07 13:52:26 -0500

    added charts

--- client/src/components/ChartsPage.js
+++ client/src/components/ChartsPage.js
@@ -1,9 +1,22 @@
 import React from "react";
+import LineChart from "./LineChart";
+import CustomSizeBarChart from "./CustomSizeBarChart.js";
+import HorizontalBarChart from "./HorizontalBarChart";
+import PieChart from "./PieChart";
 
 export default function Charts() {
-    return (
-        <div>
-            Charts go here
-        </div>
-    )
+  return (
+    <div>
+      <h1>Chart Examples</h1>
+      <hr />
+      <LineChart />
+      <hr />
+      <PieChart />
+      <hr />
+      <HorizontalBarChart />
+      <hr />
+      <CustomSizeBarChart />
+      <hr />
+    </div>
+  );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./store/reducer";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import BaseLayout from "./containers/BaseLayout";
+import ChartsPage from "./components/ChartsPage";
+
+let store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <BaseLayout>
+          <Switch>
+            {/* <Route component={ChartsPage} path="/charts/:id" exact /> */}
+            <Route component={ChartsPage} path="/charts" exact />
+            <Route component={App} path="/" exact />
+          </Switch>
+        </BaseLayout>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
