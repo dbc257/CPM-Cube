aa89c3abf5e139a366c78e89eb256ea1b5c3e542
David Carlisle <dave_0021@yahoo.com>
2020-07-07 18:16:27 -0500

    fixed typo

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -7,7 +7,7 @@ export default function HomePage() {
   // {pass in props onclick}
 
   return (
-    <div class="grid">
+    <div className="grid">
       {/* <Link to="/charts/:id"> */}
       <button style={{ gridArea: 1 / 1 / 2 / 2 }}>Button 1</button>
       <button style={{ gridArea: 2 / 1 / 3 / 2 }}>Button 2</button>
