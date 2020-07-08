f8918dbba6737ad11f773c45fbfa3dd10ff2dd51
Victor Petsev <Vpetsev96@gmail.com>
2020-07-08 11:13:04 -0500

    vertical carousel and scss styling

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -6,12 +6,57 @@ import "./LandingPage.css";
 export default function HomePage() {
   // {pass in props onclick}
 
+  const emojis = [
+                  ["🐳", "spouting whale", "U+1F433	"], 
+                  ["🐋", "whale", "U+1F40B	"], 
+                  ["🐬", "dolphin", "U+1F42C	"], 
+                  ["🐟", "fish", "U+1F41F	"], 
+                  ["🐠", "tropical fish", "U+1F420	"], 
+                  ["🐡", "blowfish", "U+1F421	"], 
+                  ["🦈", "shark", "U+1F988	"], 
+                  ["🐙", "octopus", "U+1F419	"], 
+                  ["🐚", "spiral shell", "U+1F41A	"]
+  ]
+
+  emojis.map(e => e[0])
   return (
-    <div className="grid">
-      {/* <Link to="/charts/:id"> */}
-      <button style={{ gridArea: 1 / 1 / 2 / 2 }}>Button 1</button>
-      <button style={{ gridArea: 2 / 1 / 3 / 2 }}>Button 2</button>
-      {/* </Link> */}
+    <div className="wrapper">
+      <div className="carousel">
+        {emojis.map(e => {
+          return (
+            <div className="carousel__item">
+              <div className="carousel__item-head">
+                {e[0]}
+              </div>
+              <div className="carousel__item-body">
+                <p className="title">{e[1]}</p>
+                <p className="Unicode">{e[2]}</p>
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
+
+
+// .wrapper
+//   .carousel
+//     - emojis = [ ["🐳", "spouting whale", "U+1F433	"], 
+//                  ["🐋", "whale", "U+1F40B	"], 
+//                  ["🐬", "dolphin", "U+1F42C	"], 
+//                  ["🐟", "fish", "U+1F41F	"], 
+//                  ["🐠", "tropical fish", "U+1F420	"], 
+//                  ["🐡", "blowfish", "U+1F421	"], 
+//                  ["🦈", "shark", "U+1F988	"], 
+//                  ["🐙", "octopus", "U+1F419	"], 
+//                  ["🐚", "spiral shell", "U+1F41A	"] ]
+    
+//     - emojis.each do |i|
+//       .carousel__item
+//         .carousel__item-head
+//           #{i[0]}
+//         .carousel__item-body
+//           %p.title #{i[1]}
+//           %p Unicode: #{i[2]}
