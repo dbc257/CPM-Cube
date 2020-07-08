796a421fc4e9fc50a96d7b944711a1a07cb8e866
David Carlisle <dave_0021@yahoo.com>
2020-07-08 16:02:38 -0500

    update

--- client/src/components/login/register.jsx
+++ client/src/components/login/register.jsx
@@ -20,10 +20,6 @@ export class Register extends React.Component {
               <input type="text" name="username" placeholder="username" />
             </div>
             <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" placeholder="email" />
-            </div>
-            <div className="form-group">
               <label htmlFor="password">Password</label>
               <input type="text" name="password" placeholder="password" />
             </div>
