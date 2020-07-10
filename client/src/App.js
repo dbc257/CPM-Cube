e086237c0c0dcdcb096fb12530507d05b874fbc1
Travis Wright <sivartwright@gmail.com>
2020-07-10 13:53:52 -0500

    display financial data

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import LandingPage from "./components/LandingPage";
+import FinanceList from "./components/FinanceList";
 
 function App() {
   return (
     <div>
+      <FinanceList />
       <LandingPage />
     </div>
   );
