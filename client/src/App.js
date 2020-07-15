ae6dc750faac0cbf06ab539654dae847bdf6f044
Victor Petsev <Vpetsev96@gmail.com>
2020-07-15 13:02:56 -0500

    updated gitignore

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import LandingPage from "./components/pages/HomePage";
+import HomePage from "./components/pages/HomePage";
 
 function App() {
   return (
